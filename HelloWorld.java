754718b0cbbc3828980282d6935a70587dd03d12
DR. KAMALJIT SINGH SAINI <27603378+kamaljitsingh76@users.noreply.github.com>
2020-08-19 11:43:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
+  public static void main(String[] args) 
+  {
+    Box b=new Box(10,4,2);
+    b.display();
   }
 }
