4e3bb5e10f443b0c46247dc3d8d0fa12686ecf43
DR. KAMALJIT SINGH SAINI <27603378+kamaljitsingh76@users.noreply.github.com>
2020-08-19 11:42:36 +0530

    Create class Box.java

---
+++ class Box.java
@@ -0,0 +1,16 @@
+class Box
+{
+  int l,b,w;
+  Box(int x, int y, int z)
+  {
+    l=x;
+    b=y;
+    w=z;
+  }
+  display()
+  {
+    System.out.println("length="+l);
+    System.out.println("breadth="+b);
+    System.out.println("width="+w);
+  }
+}
